4eef314c8dc458b737ca53aa2d421bf625e440a7
17063 <sumeyrakaptan@gmail.com>
2021-11-10 17:24:55 -0500

    WorldPopulationXpathTest.java

--- src/test/java/com/day05_xpath_css_isdisplayed/WorldPopulationXpathTest.java
+++ src/test/java/com/day05_xpath_css_isdisplayed/WorldPopulationXpathTest.java
@@ -6,16 +6,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class WorldPopulationXpathTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String appUrl = "https://www.worldometers.info/world-population";
         WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.get(appUrl);
-
-
+        Thread.sleep(4000);
         //<div class="maincounter-number">
+        WebElement worldPopulation = driver.findElement(By.xpath("//div[@class='maincounter-number']"));
 
-        WebElement worldPopulation = driver.findElement(By.xpath("//div[@]"));
         System.out.println(worldPopulation.getText());
+
+        for(int i = 1; i <= 10; i++) {
+            Thread.sleep(1000);
+            System.out.println(i + " - " + worldPopulation.getText());
+        }
+
+        driver.quit();
+
     }
 }
