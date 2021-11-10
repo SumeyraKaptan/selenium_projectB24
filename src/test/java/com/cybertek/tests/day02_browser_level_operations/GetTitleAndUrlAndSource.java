d8acf43953691d59b505e7b723b45fb24b234ff4
17063 <sumeyrakaptan@gmail.com>
2021-11-10 17:58:07 -0500

    GetTitleAndUrlAndSource.java

--- src/test/java/com/cybertek/tests/day02_browser_level_operations/GetTitleAndUrlAndSource.java
+++ src/test/java/com/cybertek/tests/day02_browser_level_operations/GetTitleAndUrlAndSource.java
@@ -4,24 +4,22 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.sql.SQLOutput;
-
 public class GetTitleAndUrlAndSource {
     public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();//Boni garcia
         //System.setProperty("webdriver.chrome.driver","/Users/cybertekb24/Downloads/chromedriver");
         String url = "https://www.etsy.com";
-        WebDriver  driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
         driver.get(url);
-        
+
         //read the title of the page and print out
-        System.out.println("Title:" + driver.getTitle());
+        System.out.println("Title: " + driver.getTitle());
         //store the title into a String variable
         String etsyTitle = driver.getTitle();
-        System.out.println("etsyTitle = " + etsyTitle);
+        System.out.println("etsyTitle = " + etsyTitle); //soutv
 
         //read the url of the current page
-        System.out.println("Current url:" + driver.getCurrentUrl());
+        System.out.println("Current url: " + driver.getCurrentUrl());
         //store the url into a String variable currentUrl
         String currentUrl = driver.getCurrentUrl();
         System.out.println("currentUrl = " + currentUrl);
@@ -34,5 +32,6 @@ public class GetTitleAndUrlAndSource {
 
         //close the browser: driver.close() , driver.quit();
         driver.quit();
+
     }
 }
